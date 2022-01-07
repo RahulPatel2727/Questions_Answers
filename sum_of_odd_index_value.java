fdf693b3eba78df4b29f876a27b56870bf5612e5
Rahul Patel <93973910+RahulPatel2727@users.noreply.github.com>
2022-01-07 17:07:58 +0530

    Create sum_of_odd_index_value.java

---
+++ sum_of_odd_index_value.java
@@ -0,0 +1,19 @@
+//package Calss_Questions;
+import java.util.Scanner;
+public class find_odd_index_sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.printf("Enter the size of the array:");
+        int size = sc.nextInt();
+        int arr[]= new int[size];
+        System.out.println("Enter the elements of array:");
+        for(int i = 0; i<arr.length; i++){
+            arr[i]=sc.nextInt();
+        }
+        int sum=0;
+        for(int i=1;i<arr.length;i+=2) {
+            sum+=arr[i];
+        }
+        System.out.println("Odd index sum is:"+sum);
+    }
+}
