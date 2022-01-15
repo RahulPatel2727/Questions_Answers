1a0789b2295d48d6dd60abb6d08d303012e37f9a
Rahul Patel <93973910+RahulPatel2727@users.noreply.github.com>
2022-01-15 19:27:07 +0530

    Create delete_consecutive_duplicate.java

---
+++ delete_consecutive_duplicate.java
@@ -0,0 +1,42 @@
+/*
+Q. Write a program to remove consecutive duplicate elements in an array.
+
+Input values
+
+10 10 30 30 40 50 50 50 9 45
+
+Output
+
+10 30 40 50 9 45
+ */
+import java.util.*;
+public class remove_consecutive_duplicates {
+    public static void main(String[] args) {
+//        int arr[] = {10 , 10 , 20 , 30 , 30 , 30 , 10};
+        System.out.println("Size");
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int arr[]= new int[size];
+        System.out.println("Enter the elements");
+        for(int i = 0; i<size; i++){
+            arr[i]=sc.nextInt();
+        }
+        int count = 0;
+        for(int i = 0; i<arr.length-count;){
+            if (arr[i]==arr[i+1]){
+                int j;count++;
+                for( j = i; j<arr.length-count; j++){
+                    arr[j]=arr[j+1];
+                }
+                arr[j]=Integer.MAX_VALUE;
+            }
+            else {
+                i++;
+            }
+        }
+        System.out.println("After delete consecutive duplicates.......");
+        for(int i= 0; i<arr.length - count; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
