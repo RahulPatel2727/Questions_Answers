397e68f97f0da9cf5808784a6d94a3aed82d1bc7
rahulpatel2727 <rahul23131420@gmail.com>
2022-06-20 16:16:36 +0530

    with order and efficient method

---
+++ allPrimeV1.java
@@ -0,0 +1,22 @@
+public class allPrimeV1 {
+    public static void main(String[] args) {
+        int n = 71;
+        for(int i=2;i<=n;i++){
+            if(isPrime(i)){
+                System.out.print(i+" ");
+            }
+        }
+
+    }
+    public static boolean isPrime(int n){
+        if(n<=1) return false;
+        if(n==2||n==3) return true;
+        if(n%2==0||n%3==0) return false;
+        for(int i=5; i*i<=n;i+=6){
+            if(n%i==0||n%(i+2)==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
