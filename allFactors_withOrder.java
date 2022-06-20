ea7cde859811ec59efdc1a2f571513ca1f5ad250
rahulpatel2727 <rahul23131420@gmail.com>
2022-06-20 15:54:28 +0530

    without order

---
+++ allFactors_withOrder.java
@@ -0,0 +1,18 @@
+public class allFactors_withOrder {
+    public static void main(String[] args) {
+        int n = 25;
+        int i;
+        for(i=1;i*i<=n;i++) {
+            if (n % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        for(;i>=1;i--){
+            if(i==n/i)
+                continue;
+            if(n%i==0){
+                System.out.print(n/i+" ");
+            }
+        }
+    }
+}
