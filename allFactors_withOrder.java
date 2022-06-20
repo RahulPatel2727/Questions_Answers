d56dcd89c6e828ed1b93a38824213b87c7e0efbd
rahulpatel2727 <rahul23131420@gmail.com>
2022-06-20 15:57:01 +0530

    with order and efficient method

--- allFactors_withOrder.java
+++ allFactors_withOrder.java
@@ -1,5 +1,6 @@
 public class allFactors_withOrder {
     public static void main(String[] args) {
+//        this is the efficient way to find all factors of a number in increasing order
         int n = 49;
         int i;
         for(i=1;i*i<=n;i++) {
