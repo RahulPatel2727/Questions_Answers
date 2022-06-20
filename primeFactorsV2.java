23e15a00c0a5c1bbc2d61e65a732a448b539c2f9
rahulpatel2727 <rahul23131420@gmail.com>
2022-06-20 11:45:16 +0530

    new

--- primeFactorsV2.java
+++ primeFactorsV2.java
@@ -1,5 +1,26 @@
 public class primeFactorsV2 {
     public static void main(String[] args) {
-
+        int n=2483;
+        if(n<=1) return;
+        while(n%2==0){
+            System.out.print(2+" ");
+            n=n/2;
+        }
+        while(n%3==0){
+            System.out.print(3+" ");
+            n/=3;
+        }
+        for(int i=5;i*i<=n;i+=6){
+                while(n%i==0){
+                    System.out.print(i+" ");
+                    n/=i;
+                }
+                while (n%(i+2)==0){
+                    System.out.print((i+2)+" ");
+                    n/=(i+2);
+                }
+        }
+        if(n>3)
+            System.out.print(n);
     }
 }
---
+++ test.java
@@ -0,0 +1,10 @@
+public class test {
+    public static void main(String[] args) {
+        int n=2483;
+        for(int i=2;i<=n;i++){
+            if(n%i==0){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
