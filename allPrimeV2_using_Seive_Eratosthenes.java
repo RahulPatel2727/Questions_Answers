7b3761f794d52eb7b8539a2c8bd9bcba1a561e3c
rahulpatel2727 <rahul23131420@gmail.com>
2022-06-20 16:57:31 +0530

    change

--- allPrimeV2_using_Seive_Eratosthenes.java
+++ allPrimeV2_using_Seive_Eratosthenes.java
@@ -1,10 +1,13 @@
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 public class allPrimeV2_using_Seive_Eratosthenes {
-    public static void main(String[] args) {
+    public static void main(String[] args)  throws InterruptedException{
+        long s=System.nanoTime();
         int n=11;
         boolean [] fact = new boolean[n+1];
-        for(int i=2;i<=n;i++){
+        //
+        for(int i=2;i*i<=n;i++){
             if(!fact[i]){
                 for(int j=2*i;j<=n;j+=i){
                     fact[j]=true;
@@ -16,5 +19,11 @@ public class allPrimeV2_using_Seive_Eratosthenes {
                 System.out.print(i+" ");
             }
         }
+
+        //checking time taken by the method
+        TimeUnit.SECONDS.sleep(2);
+        long e=System.nanoTime();
+        System.out.println();
+        System.out.println("time "+(double) (e-s)/1000000000+"s"                            );
     }
 }
