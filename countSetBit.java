3d7fb947ec40dd9b4267ee9436d37a0179c003e2
rahulpatel2727 <rahul23131420@gmail.com>
2022-06-21 16:55:16 +0530

    change

---
+++ countSetBit.java
@@ -0,0 +1,17 @@
+public class countSetBit {
+    public static void main(String[] args) {
+        int a=15274;
+        System.out.println(new countSetBit().setBit(a));
+    }
+//    naive method
+    public int setBit(int a){
+        int count = 0;
+        while(a!=0){
+            if(a%2==1){
+                count++;
+            }
+            a/=2;
+        }
+        return count;
+    }
+}
