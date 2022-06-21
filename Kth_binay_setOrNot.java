85ec140fa3824b4e31f4e38053f2a4dfe3039c45
rahulpatel2727 <rahul23131420@gmail.com>
2022-06-21 16:34:13 +0530

    change

--- Kth_binay_setOrNot.java
+++ Kth_binay_setOrNot.java
@@ -1,8 +1,11 @@
+import java.security.spec.RSAOtherPrimeInfo;
+
 public class Kth_binay_setOrNot {
     public static void main(String[] args) {
         int a=15274,k=14;  // k always less than the max bit in a no or in a range 32/64...
         System.out.println(new Kth_binay_setOrNot().Kth(a,k));
         System.out.println(new Kth_binay_setOrNot().KthV1(a,k));
+        System.out.println(new Kth_binay_setOrNot().Kthv2(a,k));
     }
 //    naive solution
     public boolean Kth(int a, int k){
@@ -22,5 +25,12 @@ public class Kth_binay_setOrNot {
             return false;
         else
             return true;
+    }
+//    efficient using right shift
+    public boolean Kthv2(int a, int k){
+        if((a>>(k-1) & 1)==1)
+            return true;
+        else
+            return false;
     }
 }
