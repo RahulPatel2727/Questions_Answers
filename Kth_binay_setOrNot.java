99c957f8c42bb36779a6ba47332da55876c5469a
rahulpatel2727 <rahul23131420@gmail.com>
2022-06-21 16:11:46 +0530

    change

--- Kth_binay_setOrNot.java
+++ Kth_binay_setOrNot.java
@@ -1,5 +1,18 @@
 public class Kth_binay_setOrNot {
     public static void main(String[] args) {
-        
+        int a=15274,k=14;  // k always less than the max bit in a no or in a range 32/64...
+        System.out.println(new Kth_binay_setOrNot().Kth(a,k));
     }
+//    naive solution
+    public boolean Kth(int a, int k){
+        while(k!=0){
+            if(k==1 && a%2==1){
+                return true;
+            }
+            a=a/2;
+            k--;
+        }
+        return false;
+    }
+
 }
