8fb176c849bc5d7bc40030e3df813b89cc05a86a
rahulpatel2727 <rahul23131420@gmail.com>
2022-06-21 18:13:33 +0530

    change

--- test.java
+++ test.java
@@ -2,10 +2,6 @@ import java.util.concurrent.TimeUnit;
 
 public class test {
     public static void main(String[] args) throws  InterruptedException{
-        System.out.println(-2>>1);
-        long s=System.nanoTime();
-        TimeUnit.SECONDS.sleep(1);
-        long e = System.nanoTime();
-        System.out.println((double) (e-s)/1000000000);
+        int a =100;
     }
 }
