0b90ead61deb0facb5857ad132c8705fa1c028bc
rahulpatel2727 <rahul23131420@gmail.com>
2022-06-22 18:53:45 +0530

    change

--- powerOfTwo_efficient.java
+++ powerOfTwo_efficient.java
@@ -20,9 +20,6 @@ public class powerOfTwo_efficient {
 
     public boolean powOf2(int n){
         if(n==0) return false;
-        if((n&(n-1))==0)
-            return true;
-        else
-            return false;
+        return (n&(n-1))==0;
     }
 }
