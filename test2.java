8be4dc78bfc24b306aea9ae2cce00a6e5f05b0b7
rahulpatel2727 <rahul23131420@gmail.com>
2022-06-22 18:10:54 +0530

    change

--- test2.java
+++ test2.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
 public class test2 {
     public static void main(String[] args) {
-        System.out.println(0/0);
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a number to find their set Bits..");
+        int n=s.nextInt();
+        int []table=new int[256];
+        table[0]=0;
+        for(int i=1; i<256;i++){
+            table[i]=(i&1)+table[i/2];
+        }
+        System.out.println("Set Bits are "+new test2().setBit(n,table));
+    }
+    public int setBit(int n,int []table){
+        int count=0,size=4;
+        while(size-->0){
+            count+=table[n&255];//0xff
+            n>>=8;
+        }
+        return count;
     }
 }
