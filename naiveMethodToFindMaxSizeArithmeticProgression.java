586614295d02e43ccdaa39393386297b5cecffa9
rahulpatel2727 <rahul23131420@gmail.com>
2022-06-22 17:37:49 +0530

    change

--- naiveMethodToFindMaxSizeArithmeticProgression.java
+++ naiveMethodToFindMaxSizeArithmeticProgression.java
@@ -1,6 +1,6 @@
 import java.util.concurrent.TimeUnit;
 
-public class naiveMethodToFindMaxSizeArithmaticProgression {
+public class naiveMethodToFindMaxSizeArithmeticProgression {
     public static void main(String[] args) throws InterruptedException {
         int[] A ={1,2,5,6,7,10,11,12,15,16,17,20,21,22,23};//{1,2,5,6,7,10,11,12,13,15,16,17,20,21,22,25,26,27,30,31,32,35,36};// 7
         int n=A.length;
