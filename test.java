a05277e6677a0b513f796dced3613a8cc3b56e39
rahulpatel2727 <rahul23131420@gmail.com>
2022-06-22 11:06:50 +0530

    change

--- test.java
+++ test.java
@@ -2,7 +2,7 @@ import java.util.concurrent.TimeUnit;
 
 public class test {
     public static void main(String[] args) throws InterruptedException {
-        int[] A = {1,1,1,1,1,1,1};//{1,4,7,10,11,12,13,16,19};//{1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,3,4,5,6,7,8,9};//{1,2,5,6,7, 9, 11, 13, 15, 17, 19,111};
+        int[] A = {1,2,5,6,7,10,11,12,13,15,16,17,20,21,22,25,26, 27,30,31,32,35,36};// 7
         int n=A.length;
         int count=0,temp=Integer.MIN_VALUE;
         for(int i=0; i<n-1;i++){
@@ -12,7 +12,7 @@ public class test {
             while(m!=0){
                 if(A[m]==(a-h*dif)){
                     h++;
-                    ind++;
+                    ind--;
                 }
                 m--;
             }
@@ -30,6 +30,7 @@ public class test {
             }
             temp=dif;
         }
+
 //        int n = A.length;
 //        int count=0, temp = Integer.MIN_VALUE;
 //        for (int i=0; i<n-1; i++) {
