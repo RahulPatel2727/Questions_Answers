a992a9e60c5f416db47fbb99e01e603203476ce7
rahulpatel2727 <rahul23131420@gmail.com>
2022-06-22 19:40:03 +0530

    change

--- oddOccurrence.java
+++ oddOccurrence.java
@@ -1,8 +1,10 @@
 public class oddOccurrence {
     public static void main(String[] args) {
-        int []arr={3,3,3,3,2,2,4,5,4,4,5,0,4};
+        int []arr={3,3,3,3,2,2,4,5,4,4,5,0,4,0,9,9,9,9,9,9,9};
         System.out.println(new oddOccurrence().occurrence(arr));
+        System.out.println(new oddOccurrence().occurrence2(arr,0,0));
     }
+//    naive solution
     public int occurrence(int []arr){
         for(int i=0;i<arr.length;i++){
             int count=0;
@@ -14,5 +16,11 @@ public class oddOccurrence {
                 return arr[i];
         }
         return -1;
+    }
+//    efficient solution using XOR
+    public int occurrence2(int []arr,int l,int res){
+        if(l==arr.length)
+            return res;
+        return occurrence2(arr,l+1,res^arr[l]);
     }
 }
