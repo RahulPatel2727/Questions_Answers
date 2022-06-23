a493584513369e39f6374fb0972f7bb61102ccef
rahulpatel2727 <rahul23131420@gmail.com>
2022-06-23 06:36:57 +0530

    change

--- test2.java
+++ test2.java
@@ -5,11 +5,6 @@ import java.util.Set;
 
 public class test2 {
     public static void main(String[] args) {
-        int m=11;int c=0;
-        while(m>0){
-            c++;
-            m/=2;
-        }
-        System.out.println(c);
+        System.out.println();
     }
 }
