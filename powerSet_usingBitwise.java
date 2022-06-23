d2a6130218138236663422f5afb5627ddb2d80ed
rahulpatel2727 <rahul23131420@gmail.com>
2022-06-23 20:06:12 +0530

    change

--- powerSet_usingBitwise.java
+++ powerSet_usingBitwise.java
@@ -2,8 +2,13 @@ public class powerSet_usingBitwise {
     public static void main(String[] args) {
         String s = "abc";
         int n = s.length();
+        int []arr={1,2,3};
+//        for string
         new powerSet_usingBitwise().powSet(s,n);
+//        for array
+//        new powerSet_usingBitwise().powSet(arr,arr.length);
     }
+    //for string
     public void powSet(String str,int n){
         int pow=(int)Math.pow(2,n);
         for(int i=0;i<pow;i++){
@@ -11,7 +16,19 @@ public class powerSet_usingBitwise {
                 if((i&(1<<j))!=0)
                     System.out.print(str.charAt(j));
             }
-            System.out.println(" ");
+            System.out.println();
+        }
+    }
+    // for array
+    public void powSet(int []arr,int len){
+        int pow=(int) Math.pow(2,len);
+        for(int i=0;i<pow;i++){
+            for(int j=0;j<len;j++){
+                if((i&(1<<j))!=0){
+                    System.out.print(arr[j]);
+                }
+            }
+            System.out.println();
         }
     }
 }
