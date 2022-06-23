3bbd0bfeff68ec8bad320ac44634270d72d8b2f3
rahulpatel2727 <rahul23131420@gmail.com>
2022-06-23 18:36:03 +0530

    change

--- test2.java
+++ test2.java
@@ -1,11 +1,16 @@
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class test2 {
     public static void main(String[] args) {
-        System.out.println(9^3);
-        System.out.println(10&(~(9)));
+        List<List<Integer>>list=new ArrayList<>();
+        List l=new ArrayList();
+        l.add(1);
+        l.add(3);
+        l.add(4);
+        list.add(l);
+        list.add(l);
+        list.add(l);
+
+        System.out.println(list);
     }
 }
