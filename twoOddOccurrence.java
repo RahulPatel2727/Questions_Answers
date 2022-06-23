bcac7e3383ad180e441ac4c20a8d4338c2246ae6
rahulpatel2727 <rahul23131420@gmail.com>
2022-06-23 16:53:18 +0530

    change

--- twoOddOccurrence.java
+++ twoOddOccurrence.java
@@ -1,6 +1,6 @@
 public class twoOddOccurrence {
     public static void main(String[] args) {
-        int []arr={1,2,3,4,5,5,4,3,2,1,1,1,6,8};
+        int []arr={1,2,3,4,5,5,4,3,2,1,1,1,6,8,8};
         int []res=new twoOddOccurrence().twoOddOcc(arr);
         System.out.println(res[0]+" "+res[1]);
     }
