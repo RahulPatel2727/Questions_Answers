5a6ebbb242dc45ba2230fb1ebff1dd22afddb434
rahulpatel2727 <rahul23131420@gmail.com>
2022-06-23 15:31:41 +0530

    change

--- test2.java
+++ test2.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 public class test2 {
     public static void main(String[] args) {
-        System.out.println(32&(~(31)));
+        System.out.println(9^3);
+        System.out.println(10&(~(9)));
     }
 }
