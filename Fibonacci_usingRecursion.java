26a0cd2217b24649318b9a198c62aa6bc7435d78
rahulpatel2727 <rahul23131420@gmail.com>
2022-06-24 11:48:14 +0530

    change

--- Fibonacci_usingRecursion.java
+++ Fibonacci_usingRecursion.java
@@ -8,6 +8,8 @@ public class Fibonacci_usingRecursion {
             return 0;
         if(n==1)
             return 1;
+//        instead of both above condition we make it as one line
+//        if(n<=1) return n;
         return Nth(n-1)+Nth(n-2);
     }
 }
