05c2b2cadde77235bde4d7004059e3f2fe7168c9
rahulpatel2727 <rahul23131420@gmail.com>
2022-06-24 10:58:39 +0530

    change

--- recursion_factorial.java
+++ recursion_factorial.java
@@ -1,8 +1,14 @@
 public class recursion_factorial {
     public static void main(String[] args) {
         int n=5;
-        System.out.println(notTail(n));
-
+//        System.out.println(notTail(n));
+        System.out.println(Tail(n,1));
+    }
+//    convert notTail recursion to Tail recursion
+    public static int Tail(int n,int res){
+        if(n==0||n==1)
+            return res;
+        return Tail(n-1,n*res);
     }
 
 //    this is not a tail recursion
