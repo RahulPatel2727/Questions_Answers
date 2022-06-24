724df0a2366c05ee133964ebc688e664111b649a
rahulpatel2727 <rahul23131420@gmail.com>
2022-06-24 09:40:38 +0530

    change

--- recursion.java
+++ recursion.java
@@ -1,9 +1,20 @@
 public class recursion {
     public static void main(String[] args) {
-        int n=16;
+        int n=10;
 //        p1(n);
-        System.out.println(p2(n));
+//        System.out.println(p2(n));
+        p3(n);
     }
+
+    // this recursion find bits
+    public static void p3(int n){
+        if(n==0)
+            return ;
+        p3(n/2);
+        System.out.print(n%2);
+    }
+
+    //this recursion find floor of log n base 2, you can change the base and base case accordingly
     public static int p2(int n){
         if(n==1)
             return 0;
@@ -11,6 +22,7 @@ public class recursion {
         return nm;
     }
 
+    // recursion analysis
     static void p1(int n){
         if(n==0)
             return;
